2593a775c0c154ef6b5bcc1890fe0179e94a584c
Hannah Lee <013hannah.a.lee@gmail.com>
2021-04-21 16:27:36 -0500

    consolodated schemas into one, removed unnecessary seed folder

--- EmployeeManager-Schema.sql
+++ EmployeeManager-Schema.sql
@@ -3,6 +3,18 @@ CREATE database employee_db;
 
 USE employee_db;
 
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roleTable (
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
 CREATE TABLE employee (
   id INT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -11,4 +23,7 @@ CREATE TABLE employee (
   manager_id  INT NULL
 );
 
+
+SELECT * FROM DepartmentTable;
+SELECT * FROM RoleTable;
 SELECT * FROM EmployeeTable;
